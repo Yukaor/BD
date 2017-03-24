7aac125371c6182ea0951290f9dcf1d3ccff70c9
Yuks <beck.nicolas0@gmail.com>
2017-03-24 11:49:47 +0100

    modification du menu

--- app/src/main/java/com/madmaxi/bd/MainActivity.java
+++ app/src/main/java/com/madmaxi/bd/MainActivity.java
@@ -2,6 +2,8 @@ package com.madmaxi.bd;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button newButton = (Button) findViewById(R.id.newButton);
+        final Button resumeButton = (Button) findViewById(R.id.resumeButton);
+        final Button copyrightsButton = (Button) findViewById(R.id.copyrightsButton);
+
+        newButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                newAction();
+            }
+        });
+        resumeButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                resumeAction();
+            }
+        });
+        copyrightsButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                copyrightsAction();
+            }
+        });
+    }
+
+    private void newAction(){
+        setContentView(R.layout.activity_new);
+    }
+    private void resumeAction(){
+        setContentView(R.layout.activity_resume);
+    }
+    private void copyrightsAction(){
+        setContentView(R.layout.activity_copyrights);
     }
 }
