789a5f32efca722310b3f189e97b32bb25a24aa0
Benjaminrivard <rivard.benjamin.28@gmail.com>
2017-03-31 09:21:31 +0200

    p^p,

--- app/src/main/java/com/madmaxi/bd/MainActivity.java
+++ app/src/main/java/com/madmaxi/bd/MainActivity.java
@@ -4,6 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -12,6 +15,7 @@ import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView texte;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
             // do reading, usually loop until end of file reading
             String mLine;
             while ((mLine = reader.readLine()) != null) {
-                
+                (TextView)texte = findViewById(R.id.textView);
 
             }
         } catch (IOException e) {
