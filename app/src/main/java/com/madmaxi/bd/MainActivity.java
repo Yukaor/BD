5cd87585cdd9aa6e9b26f42e971e0d2b01ff534c
Benjaminrivard <rivard.benjamin.28@gmail.com>
2017-04-28 09:38:41 +0200

    dzqqdq

--- app/src/main/java/com/madmaxi/bd/MainActivity.java
+++ app/src/main/java/com/madmaxi/bd/MainActivity.java
@@ -1,11 +1,14 @@
 package com.madmaxi.bd;
 
 import android.content.res.AssetManager;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.view.View;
 
 import org.w3c.dom.Document;
 
@@ -18,14 +21,30 @@ import javax.xml.parsers.DocumentBuilderFactory;
 public class MainActivity extends AppCompatActivity {
 
     TextView texteView;
-    ImageView ImageView;
-    String Vignette = "1-1";
+    ImageView imageView;
+    Button next;
+    int Vignette = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Affection du fichier de layout
         setContentView(R.layout.activity_main);
 
+        //Obtention des références sur les composants
         texteView = (TextView) findViewById(R.id.textView);
+        imageView = (ImageView) findViewById(R.id.imageView);
+        next = (Button) findViewById(R.id.button);
+
+        //Lorsque l'on appuie sur le bouton next, on change l'id de vignette.
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                Vignette += 1;
+                
+                        
+            }
+        });
 
         AssetManager assetManager = getAssets();
         InputStream inputStream = null;
@@ -33,7 +52,7 @@ public class MainActivity extends AppCompatActivity {
         try {
             inputStream = assetManager.open("histoire.xml");
              doc= parseXML(inputStream);
-            String textVignette = parser.getTextAtId(doc,Vignette);
+            String textVignette = parser.getTextAtId(doc, ((String) Vignette));
             texteView.setText(textVignette);
         } catch (IOException e) {
             Log.e("tag", e.getMessage());
